1b2ca2f008132a3240e0356d88d2dba4790bdaf8
leeo <47030312+leeo91@users.noreply.github.com>
2023-02-06 21:36:51 +0800

    Create _13_evalFunc.py

---
+++ _13_evalFunc.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Auther : Zhichao
+# Date : 2023/1/14 21:49
+# File : eval.py
+
+s = '3.14+3'
+x = eval(s)
+print(x, type(x))
+
+name = input("pls input name:")
+print(name)
+
+age = eval(input('pls input age:'))
+print(age, type(age))
+
+height = eval(input('pls input height:'))
+print(age, type(height))
