962de3907c1eff9d5095a96196e41b73de57cb0b
leeo <47030312+leeo91@users.noreply.github.com>
2023-02-06 21:22:13 +0800

    Create _11_bool.py

---
+++ _11_bool.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Auther : Zhichao
+# Date : 2023/1/13 20:43
+# File : bool_test.py
+print(int(True))
+print(int(False))
+
+print(type(True))
+print(True + 10)
+print(False + 10)
+
+
+# test the object's bool value
+print(bool(18))
+print(bool(0), bool(0.0))
+print(bool('beijing'))
+print(bool(False))
+print(bool(None))
