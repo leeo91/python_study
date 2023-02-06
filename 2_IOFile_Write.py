558c733b18bde176decbf87d2913a3c8d71a6459
leeo <47030312+leeo91@users.noreply.github.com>
2023-02-06 10:42:40 +0800

    Create 2_IOFile_Write.py

---
+++ 2_IOFile_Write.py
@@ -0,0 +1,7 @@
+file=input('pls input: ')
+print('the input value is: ',file)
+
+file = open('note.txt','w')
+print('beijing, welcome', "hello",sep = '\t xxx', end = '\r --->', file=file)
+file.close
+
