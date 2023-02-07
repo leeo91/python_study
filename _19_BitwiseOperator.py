c048eae9f90f578376c263c930f3a50f5c6d361b
leeo <47030312+leeo91@users.noreply.github.com>
2023-02-07 12:09:42 +0800

    Create _19_BitwiseOperator.py

---
+++ _19_BitwiseOperator.py
@@ -0,0 +1,19 @@
+#1. & (1100 & 1000 = 1000) bitwise and
+print(12&8)
+
+#2. | (0100 | 1000 = 1100) bitwise or
+print(4|8)
+
+#3. ^ (11111 | 10110 = 01001) bitwise exclusive or
+print(31^22)
+
+#4. ~ (~1111011 = 1111111110000100) bitwise negate
+print(~123)
+
+#5. << (0010 << 2) left shift
+print(2 << 2)
+
+#6. >> (0010 >> 2) right shift
+print(bin(-8))
+print(bin(-2))
+print(-8 >> 2)
