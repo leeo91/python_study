30dc0124abd670547b199e4a72716183b7a58789
leeo <47030312+leeo91@users.noreply.github.com>
2023-02-07 11:27:43 +0800

    Create _15_ArithmaticOperator.py

---
+++ _15_ArithmaticOperator.py
@@ -0,0 +1,11 @@
+print('plus:', 1+1)
+print('minus:', 1-1)
+print('multiply:', 1*1)
+print('divide:', 1/1)  #implicit conversion
+print('integer division:', 10//3)
+print('integer modular:', 10%3)
+print('exponentiation:', 2**3)
+
+# 1. **
+# 2. *,/,%,//
+# 3. +,-
