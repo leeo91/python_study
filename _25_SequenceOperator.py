5c39e82003195b5d3f55024c24ddb93476038a02
leeo <47030312+leeo91@users.noreply.github.com>
2023-02-08 14:24:20 +0800

    Create _25_SequenceOperator.py

---
+++ _25_SequenceOperator.py
@@ -0,0 +1,26 @@
+s1='hello'
+s2='world'
+print(s1+s2)
+print(s1*3)
+
+lst1=['h']
+lst2=['w']
+print(lst1+lst2)
+print(lst1*3)
+#print(s1+lst1)  #TypeError
+
+print('-'*40)
+
+s='helloworld'
+print('e' in s)
+print('v' in s)
+print('e' not in s)
+print('v' not in s)
+
+print(len(s))
+print(max(s))
+print(min(s))
+
+
+print(s.index('o'))
+print(s.count('o'))
