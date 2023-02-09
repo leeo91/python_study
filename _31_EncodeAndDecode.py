82643e721bc169eb23c28f6eefb7f2544111c1c3
leeo <47030312+leeo91@users.noreply.github.com>
2023-02-09 16:23:20 +0800

    Create _31_EncodeAndDecode.py

---
+++ _31_EncodeAndDecode.py
@@ -0,0 +1,15 @@
+s='伟大的中国梦'
+scode_gbk=s.encode('gbk')
+print(scode_gbk)
+scode_utf8=s.encode('utf-8')
+print(scode_utf8)
+
+s2='耶๑'
+#errors -> ignore/strict/replace
+scode=s2.encode('gbk',errors='replace')
+print(scode)
+
+
+print(bytes.decode(scode_gbk,'gbk'))
+print(bytes.decode(scode_utf8,'utf-8'))
+print(bytes.decode(scode,'gbk',errors='ignore'))
